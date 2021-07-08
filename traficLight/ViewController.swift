d2be308bb71496b040e4d5236ffd8b52bb18272a
Andrii Oliievskyi <androlyk@gmail.com>
2021-07-07 22:02:28 -0700

    FINAL COMPLETED HW_2.1.2

--- traficLight/ViewController.swift
+++ traficLight/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 15
+       
     }
 
 
+    @IBAction func startButtonPressed() {
+        if redLightView.alpha != 1 && yellowLightView.alpha != 1 && greenLightView.alpha != 1 {
+            redLightView.alpha = 1
+            startButton.setTitle("NEXT", for: .normal)
+        } else if redLightView.alpha == 1 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        } else if yellowLightView.alpha == 1 {
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        } else if greenLightView.alpha == 1 {
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+        }
+    }
+    
 }
+
 
